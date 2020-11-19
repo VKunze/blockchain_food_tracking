26952cb57dff25d1a334478b5220b015d268f07e
VKunze <39528476+VKunze@users.noreply.github.com>
2020-11-18 22:05:59 -0300

    Llamar func desde boton

--- web/main/main.js
+++ web/main/main.js
@@ -1,16 +1,19 @@
 lastIndex = 0
 
-if (window.ethereum) {
-    window.web3 = new Web3(window.ethereum);
-    window.ethereum.enable();
- 
-    window.cajon = new window.web3.eth.Contract(abi, '0xDbd130C0010CB3948fc7e38Bbc0afE1d93a33cf8');
 
-    window.cajon.events.Transfer({}, function(error, event) {
-        console.log(event);
-    });
-    console.log(obtenerCuentas())
 
+function start_bc(abi){
+    if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        window.ethereum.enable();
+
+        window.cajon = new window.web3.eth.Contract(abi, '0xDbd130C0010CB3948fc7e38Bbc0afE1d93a33cf8');
+
+        window.cajon.events.Transfer({}, function(error, event) {
+            console.log(event);
+        });
+        console.log(obtenerCuentas())
+    }
 }
 
 async function obtenerCuentas() {
@@ -19,6 +22,7 @@ async function obtenerCuentas() {
 }
 
 async function agregarAWhitelist(address, tipoWhitelist) {
+    console.log("in main")
     if(tipoWhitelist == "productores"){
         window.cajon.methods.addToWhitelistProductores(address).send();
     } else if(tipoWhitelist == "distribuidores"){
@@ -36,7 +40,7 @@ async function crearCajon(tipoContenido, nombreComercialProductor) {
     console.log("ee")
 }
 
-function agregarPuntoCadena(tokenId, nombreComercial) {
+async function agregarPuntoCadena(tokenId, nombreComercial) {
     cuenta = await obtenerCuentas()
     window.cajon.methods.agregarPuntoCadena(tokenId, cuenta[1], nombreComercial)
 }
@@ -45,3 +49,12 @@ function obtenerIndex(){
     lastIndex= lastIndex+1
     return lastIndex
 }
+
+document.addEventListener("DOMContentLoaded", async function(event) {
+    document.querySelector("#registrarProductor").addEventListener("click", async function() {
+        var address = document.getElementById("address").value
+        var tipoWhitelist = document.getElementById("rubro").value
+        console.log("in agregar prod")
+        agregarAWhitelist(address, tipoWhitelist)
+    });
+})
