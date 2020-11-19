392258a670b28d66b86f2babd9d37cf85d9bae73
VKunze <39528476+VKunze@users.noreply.github.com>
2020-11-19 10:33:38 -0300

    Document fixed

--- web/main/main.js
+++ web/main/main.js
@@ -1,22 +1,19 @@
 lastIndex = 0
 
-exports.arrancarSmartContract = (abi) => {
-    
-}
 
 async function obtenerCuentas() {
     var cuentas = await window.web3.eth.getAccounts();
-    return cuentas; 
+    return cuentas;
 }
 
 async function agregarAWhitelist(address, tipoWhitelist) {
     console.log("in main")
-    if(tipoWhitelist == "productores"){
+    if (tipoWhitelist == "productores") {
         window.cajon.methods.addToWhitelistProductores(address).send();
-    } else if(tipoWhitelist == "distribuidores"){
-        window.cajon.methods.addToWhitelistDistribuidores(address).send();    
-    } else if(tipoWhitelist == "puntosVenta"){
-        window.cajon.methods.isInWhitelistPuntosVenta(address).send();    
+    } else if (tipoWhitelist == "distribuidores") {
+        window.cajon.methods.addToWhitelistDistribuidores(address).send();
+    } else if (tipoWhitelist == "puntosVenta") {
+        window.cajon.methods.isInWhitelistPuntosVenta(address).send();
     }
 }
 
@@ -28,7 +25,7 @@ async function crearCajon(tipoContenido, nombreComercialProductor) {
     console.log("ee")
 }
 
-async function obtenerCajon(tokenId){
+async function obtenerCajon(tokenId) {
     [id, tipoContenido, trayecto] = await window.cajon.methods.obtenerCajon(tokenId)
     return (id, tipoContenido, trayecto)
 }
@@ -38,37 +35,42 @@ async function agregarPuntoCadena(tokenId, nombreComercial) {
     window.cajon.methods.agregarPuntoCadena(tokenId, cuenta[1], nombreComercial)
 }
 
-function obtenerIndex(){
-    lastIndex= lastIndex+1
+function obtenerIndex() {
+    lastIndex = lastIndex + 1
     return lastIndex
 }
 
-document.addEventListener("DOMContentLoaded", async function(event) {
-    document.querySelector("#registrarProductor").addEventListener("click", async function() {
-    console.log("LLLLLLEEEEEEEEGGGGGGGGGGGOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo")
-        var address = document.getElementById("address").value
-        var tipoWhitelist = document.getElementById("rubro").value
-        console.log("in agregar prod")
-        await agregarAWhitelist(address, tipoWhitelist)
-        console.log("SUCCESS!")
-    });
-
-    document.addEventListener("#agregarUbicacion").addEventListener("click", async function() {
-        tokenId = document.getElementById("???").innerHTML
-        nombreComercial =  document.getElementById("???").innerHTML
-        await agregarPuntoCadena(tokenId, nombreComercial)
-
-    });
-
-    document.querySelector("#buscarLote").addEventListener("click", async function() {
-        console.log("HI")
-        var tokenId = document.getElementById("idToken").value
-        [id, tipoContenido, trayecto]= await obtenerCajon(tokenId)  
-        document.getElementById("detallesCajon").style.display = "block"
-        document.getElementById("tipoContenidoCajon").innerHTML= tipoContenido
-        document.getElementById("trayectoCajon").innerHTML= trayecto
-        
-        
-    });
+document.addEventListener("DOMContentLoaded", async function (event) {
+    if (document.querySelector("#registrar")) {
+        console.log("Entro")
+        document.querySelector("#registrar").addEventListener("click", async function () {
+            console.log("LLLLLLEEEEEEEEGGGGGGGGGGGOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo")
+            var address = document.getElementById("address").value
+            var tipoWhitelist = document.getElementById("rubro").value
+            console.log("in agregar prod")
+            await agregarAWhitelist(address, tipoWhitelist)
+            console.log("SUCCESS!")
+        });
+    }
+    if(document.querySelector("#agregarUbicacion")){
+        document.querySelector("#agregarUbicacion").addEventListener("click", async function() {
+            tokenId = document.getElementById("???").innerHTML
+            nombreComercial =  document.getElementById("???").innerHTML
+            await agregarPuntoCadena(tokenId, nombreComercial)
+    
+        });
+    }
+    if(document.querySelector("#buscarLote")){
+        document.querySelector("#buscarLote").addEventListener("click", async function() {
+            console.log("HI")
+            var tokenId = document.getElementById("idToken").value
+            [id, tipoContenido, trayecto]= await obtenerCajon(tokenId)  
+            document.getElementById("detallesCajon").style.display = "block"
+            document.getElementById("tipoContenidoCajon").innerHTML= tipoContenido
+            document.getElementById("trayectoCajon").innerHTML= trayecto
+            
+            
+        });
+    }
 })
 
