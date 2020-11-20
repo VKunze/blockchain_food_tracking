216968d11bd507fc3901f1f8f0059365a6782789
VKunze <39528476+VKunze@users.noreply.github.com>
2020-11-20 10:27:41 -0300

    Final fixes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,5 @@
-// const ConvertLib = artifacts.require("ConvertLib");
-// const MetaCoin = artifacts.require("MetaCoin");
-// const MyERC20Token = artifacts.require("MyERC20Token");
 const Cajon = artifacts.require("Cajon");
 
 module.exports = function(deployer) {
-    // deployer.deploy(ConvertLib);
-    // deployer.link(ConvertLib, MetaCoin);
-    // deployer.deploy(MetaCoin);
-    // deployer.deploy(MyERC20Token);
     deployer.deploy(Cajon);
 };
